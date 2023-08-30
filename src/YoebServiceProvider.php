6d259cef1ca6a1128b62f1a326e3fa653abba91a
Berkay <yberkay2004@gmail.com>
2023-08-30 20:59:47 +0300

    v0.6

--- src/YoebServiceProvider.php
+++ src/YoebServiceProvider.php
@@ -1,13 +1,17 @@
 <?php
 namespace Yoeb\AddressInstaller;
 
+
 use Illuminate\Support\ServiceProvider;
+use Yoeb\AddressInstaller\Database\Seeders\DatabaseSeeder;
 
 class YoebServiceProvider extends ServiceProvider {
 
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $seed = new DatabaseSeeder();
+        $seed->run();
     }
 
 }
